1185e1dfd25c84326f8690286b5bd80a61c166a5
rlguswn <0318joo@naver.com>
2023-07-28 13:45:00 +0900

    임시 회원가입 로그인 페이지 생성

--- user/urls.py
+++ user/urls.py
@@ -6,11 +6,11 @@ app_name = 'user'
 urlpatterns = [
     # User 기능 url
     # # 회원가입 /user/register/
-    # path('register/', views.UserRegister.as_view(), name='register'),
+    path('register/', views.UserRegister.as_view(), name='register'),
     # # 로그인 /user/login/
-    # path('login/', views.UserLogin.as_view(), name='login'),
+    path('login/', views.UserLogin.as_view(), name='login'),
     # # 로그아웃 /user/logout/
-    # path('logout/', views.UserLogout.as_view(), name='logout'),
+    path('logout/', views.UserLogout.as_view(), name='logout'),
     #
     # # Profile 기능 url
     # # 프로필 /profile/
--- user/views.py
+++ user/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import redirect, render
+from .serializers import UserSerializer, LoginSerializer
 
-# Create your views here.
+
+class UserRegister(APIView):
+    def get(self, request):
+        return render(request, 'user/user_register.html')
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+
+        if serializer.is_valid():
+            user = serializer.save()
+            return Response({'message': 'User registered successfully'}, status=status.HTTP_201_CREATED)
+        return Response({'error': 'Invalid credentials'})
+
+
+class UserLogin(APIView):
+    def get(self, request):
+        return render(request, 'user/user_login.html')
+    def post(self, request):
+        if request.user.is_authenticated:
+            return redirect('blog:list')
+
+        serializer = LoginSerializer(request, request.POST)
+        if serializer.is_valid():
+            email = request.POST.get('email')
+            password = request.POST.get('password')
+
+            user = authenticate(request, email=email, password=password)
+            if user:
+                login(request, user)
+                return Response({'message': 'User logged in successfully'}, status=status.HTTP_200_OK)
+            return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
+
+
+class UserLogout(APIView):
+    def get(self, request):
+        logout(request)
+        return Response({'message': 'User logged out successfully'}, status=status.HTTP_200_OK)
